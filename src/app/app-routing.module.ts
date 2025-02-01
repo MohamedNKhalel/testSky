09ab66521d6ba0c41042cc0273537b5771f4d63d
Mohamed Khalel <mn911415@gmail.com>
2025-02-01 17:17:10 +0200

    'final'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {path:"",redirectTo:"home",pathMatch:"full"},
-  {path:"home" , loadComponent:()=>import("./components/home/home.component").then((m)=>m.HomeComponent),title:"Home"},
-  {path:"portfolio" , loadComponent:()=>import("./components/portfolio/portfolio.component").then((m)=>m.PortfolioComponent),title:"Portfolio"},
-  {path:"portfolio/:id" , loadComponent:()=>import("./components/project-details/project-details.component").then((m)=>m.ProjectDetailsComponent),title:"Details"},
-  {path:"services" , loadComponent:()=>import("./components/services/services.component").then((m)=>m.ServicesComponent),title:"Services"},
-  {path:"contact" , loadComponent:()=>import("./components/contact/contact.component").then((m)=>m.ContactComponent),title:"Contact Us"},
+  {path:"home",loadComponent:()=>import("./components/home/home.component").then(m=>m.HomeComponent),title:"Skybuilders"},
+  {path:"portfolio" , loadComponent:()=>import("./components/portfolio/portfolio.component").then((m)=>m.PortfolioComponent),title:"Skybuilders"},
+  {path:"portfolio/:id" , loadComponent:()=>import("./components/project-details/project-details.component").then((m)=>m.ProjectDetailsComponent),title:"Skybuilders"},
+  {path:"services" , loadComponent:()=>import("./components/services/services.component").then((m)=>m.ServicesComponent),title:"Skybuilders"},
+  {path:"contact" , loadComponent:()=>import("./components/contact/contact.component").then((m)=>m.ContactComponent),title:"Skybuilders"},
 ];
 
 @NgModule({
